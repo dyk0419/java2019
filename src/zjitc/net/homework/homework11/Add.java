36568776ff821a5e912542d0cc5d774efe5653ad
杜源康 <1197264772@qq.com>
2019-04-24 23:30:01 +0800

    第十一次作业

---
+++ src/zjitc/net/homework/homework11/Add.java
@@ -0,0 +1,14 @@
+package zjitc.net.homework.homework11;
+
+/**
+ * @author 杜源康
+ * @date 2019-04-24 23:27
+ * 使用Java代码操作book表：将键盘输入的内容添加到book表，
+ * 修改book表中的数据，删除数据，并且将book表中的所有记录
+ * 查询出来保存在集合中，最后遍历集合，输出所有内容。
+ */
+public class Add {
+    public static void main(String[] args) {
+
+    }
+}
